cbb07f0800619f4a1889f9c7c1822dc9e5147a91
navendra1707 <navendraagrawal17@gmail.com>
2023-12-19 20:07:35 +0530

    navbar fixes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,6 @@
-import { Box, Stack, Button } from "@mui/material";
 import React from "react";
-
+import { Box, Button, Grid } from "@mui/material";
 import WestIcon from "@mui/icons-material/West";
-
 import Heading from "../styled/Heading";
 import { useNavigate } from "react-router-dom";
 
@@ -10,43 +8,33 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <Box
-        sx={{
-          minHeight: "10vh",
-          backgroundColor: "#1f4d60",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          flexWrap={"wrap"}
-          sx={{
-            width: '90vw'
-          }}
-        >
-          <Button
-            onClick={() => {
-              navigate(-1);
-            }}
-          >
+    <Box
+      sx={{
+        minHeight: "10vh",
+        backgroundColor: "#1f4d60",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Grid container justifyContent="space-between" alignItems="center" sx={{ width: '90vw' }}>
+        <Grid item xs={3}>
+          <Button onClick={() => navigate(-1)}>
             <WestIcon sx={{ color: "#fff" }} />
           </Button>
+        </Grid>
+        <Grid item xs={6} textAlign="center">
           <Heading
-            sx={{ cursor: "pointer", color: '#fff', fontSize: '1.2rem' }}
-            onClick={() => {
-              navigate("/");
-            }}
+            sx={{ cursor: "pointer", color: "#fff", fontSize: "1.2rem" }}
+            onClick={() => navigate("/")}
           >
             Saral Nation
           </Heading>
-        </Stack>
-      </Box>
-    </div>
+        </Grid>
+        <Grid item xs={3} />
+        {/* You can adjust the xs values to control the width of the center column */}
+      </Grid>
+    </Box>
   );
 };
 
